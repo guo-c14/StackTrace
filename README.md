cecf11f754498f2ad7c172d104b6271cca0ad021
GUO-C14 <guo.c14.tech@gmail.com>
2020-02-20 20:08:43 +0800

    HW1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # StackTrace
+
+语言C#
+
+测试输出如下图：
+![](./res.png)
+
+代码见Program.cs
---
+++ StackTrace/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace StackTrace
+{
+    class Program
+    {
+        private static void test1(int a, double b)
+        {
+            test2();
+        }
+
+        private static void test2()
+        {
+            test3("test");
+        }
+
+        private static void test3(string s)
+        {
+            Console.WriteLine(GetStackTrace());
+        }
+
+        public static void Main(string[] args)
+        {
+            test1(2, 3.0);
+            Console.ReadKey();
+        }
+
+        public static string GetStackTrace()
+        {
+            System.Diagnostics.StackTrace st = new System.Diagnostics.StackTrace();
+            System.Diagnostics.StackFrame[] sfs = st.GetFrames();
+            string res = "";
+            for (int i = 1; i < sfs.Length; i++)
+            {
+                var method = sfs[i].GetMethod();
+                res += method.Name + "(";
+                var parameters = method.GetParameters();
+                int j = 0;
+                for (; j < parameters.Length - 1; j++)
+                    res += parameters[j].ParameterType + " " + parameters[j].Name + ", ";
+                for (; j < parameters.Length; j++)
+                    res += parameters[j].ParameterType + " " + parameters[j].Name;
+                res += ")\n";
+            }
+            return res;
+        }
+    }
+}
